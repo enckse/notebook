4a3afe487a85b8eb2b28a3efc5a31da13ec4ff8a
Sean Enck <enckse@voidedtech.com>
2021-11-02 10:52:50 -0400

    shellcheck cleanup

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 _parse() {
     local rel title
-    rel=$(echo "$2" | sed 's#^src#.#g')
-    title=$(cat $2 | head -n 1)
+    rel=${2/src\//\./}
+    title=$(head -n 1 "$2")
     _format "$1" "$title" "$rel"
 }
 
@@ -12,14 +12,14 @@ _format() {
 }
 
 _summary() {
-    local f dir
+    local src dir
     echo "# Summary"
     echo
     _format "" "Intro" "./intro.md"
-    for f in $(find src/ -mindepth 2 -type f -name "intro.md" | sort); do
-        _parse "" "$f"
-        dir=$(dirname $f)
-        for f in $(find $dir -type f -name "*.md" | grep -v "intro.md" | sort); do
+    for src in $(find src/ -mindepth 2 -type f -name "intro.md" | sort); do
+        _parse "" "$src"
+        dir=$(dirname "$src")
+        for f in $(find "$dir" -type f -name "*.md" | grep -v "intro.md" | sort); do
             _parse "    " "$f"
         done
     done
